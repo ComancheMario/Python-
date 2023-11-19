7780d05ea793ace10a6d9f248383dedd1f72ef11
ComancheMario <madrodriguezca@unal.edu.co>
2023-11-19 15:21:34 -0500

    graficas de oscAtm

---
+++ ProySubA/sk.atm.data.release/draw_SK_NH_C.d
@@ -0,0 +1,4 @@
+# 
+# DO NOT DELETE
+C:/Users/madro/OneDrive/Documents/.py/Python-/ProySubA/sk.atm.data.release/draw_SK_NH_C.dll: C:\root_v6.28.06\include\RVersion.h C:\root_v6.28.06\include\ROOT\RConfig.hxx C:\root_v6.28.06\include\TClass.h C:\root_v6.28.06\include\TDictAttributeMap.h C:\root_v6.28.06\include\TInterpreter.h C:\root_v6.28.06\include\TROOT.h C:\root_v6.28.06\include\TBuffer.h C:\root_v6.28.06\include\TMemberInspector.h C:\root_v6.28.06\include\TError.h C:\root_v6.28.06\include\RtypesImp.h C:\root_v6.28.06\include\TIsAProxy.h C:\root_v6.28.06\include\TFileMergeInfo.h C:\root_v6.28.06\include\TCollectionProxyInfo.h  
+draw_SK_NH_C__ROOTBUILDVERSION= "6.28/06" 
